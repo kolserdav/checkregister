1f26f035d712facebb8735ec5ab8c0f32c1a5bed
kolserdav <serega12101983@gmail.com>
2018-04-24 00:00:47 +0800

    save

--- README.md
+++ README.md
@@ -6,7 +6,27 @@ _Installation_
 
 _Using_
 
-SETTINGS:
+EXAMPLE HTML:
+```html
+<form>
+    <label for="email">Email</label><br>
+    <div id="emailError"></div>
+    <input id="email" type="email" title="input email" autocomplete="email">
+    <br>
+    <label for="password">Password</label><br>
+    <div id="passwordError"></div>
+    <input id="password" type="password" title="input password" autocomplete="new-password">
+    <br>
+    <label for="password2">Repeat password</label><br>
+    <div id="password2Error"></div>
+    <input id="password2" type="password" title="repeat password" autocomplete="new-password">
+    <br><br>
+    <div id="buttonError"></div>
+    <button id="button" type="button" title="send">Send</button>
+</form>
+```
+
+SETTINGS JS:
 ```javascript
 function CheckRegister() {}
 let check = new CheckRegister();
@@ -17,16 +37,16 @@ let check = new CheckRegister();
  */
 check.email = document.querySelector('#email');
 check.emailError = document.querySelector('#emailError');
-check.password1 = document.querySelector('#password1');
-check.password1Error = document.querySelector('#password1Error');
+check.password = document.querySelector('#password1');
+check.passwordError = document.querySelector('#password1Error');
 check.password2 = document.querySelector('#password2');
 check.password2Error = document.querySelector('#password2Error');
 check.button = document.querySelector('#button');
 check.buttonError = document.querySelector('#buttonError');
 check.minPasswordLength = 6;
-check.callbackSend = function(email, password1, password2){
+check.callbackSend = function(email, password, password2){
     console.log(email);
-    console.log(password1);
+    console.log(password);
     console.log(password2);
 };
 /**
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = c;
+//module.exports = c;
 
 function CheckRegister() {}
 let check = new CheckRegister();
@@ -10,73 +10,125 @@ let check = new CheckRegister();
  * Your elements
  * @type {Element}
  */
+    //For login
 check.email = document.querySelector('#email');
 check.emailError = document.querySelector('#emailError');
-check.password1 = document.querySelector('#password1');
-check.password1Error = document.querySelector('#password1Error');
-check.password2 = document.querySelector('#password2');
-check.password2Error = document.querySelector('#password2Error');
+check.password = document.querySelector('#password');
+check.passwordError = document.querySelector('#passwordError');
 check.button = document.querySelector('#button');
 check.buttonError = document.querySelector('#buttonError');
 check.minPasswordLength = 6;
-check.callbackSend = function(email, password1, password2){
+check.callbackSend = function(email, password1, password2){ //For example
     console.log(email);
     console.log(password1);
     console.log(password2);
 };
+    //For register
+check.password2 = document.querySelector('#password2');
+check.password2Error = document.querySelector('#password2Error');
 
 /**
  * Optional variables
  * Your messages
  */
+check.checkbox = document.querySelector('#checkbox');
+check.checkboxError = document.querySelector('#checkboxError');
+check.checkboxMessage = "Custom checkbox is required message";
 check.passwordLengthMessage = 'Custom length password message';
 check.emailError.message = 'Custom error email message';
 check.emailError.messageIfEmpty = 'Custom empty email message';
 check.passwordErrorMessage = 'Custom error password message';
 check.passwordErrorMessageIfEmpty = 'Custom empty password message';
 check.passwordsUnmatchMessage = 'Custom passwords no match message';
+check.colorErrorMesages = "red";
 
 
-function c(check)
+function c()
 {
-    let pregEmail = /\w+@\w+\.\w+/,
+    let password2 = null,
+        emailErrorOld = check.emailError.innerText,
+        passwordErrorOld = check.passwordError.innerText,
+        passwordColorOld = check.passwordError.style.color,
+        buttonErrorOld = check.buttonError.innerText,
+        checkbox = null,
+        pregEmail = /\w+@\w+\.\w+/,
         pregPass = /\w+/,
         minLength = check.minPasswordLength,
+        password2ErrorOld = '',
         defaultEmptyEmail = 'Please insert your email',
-        defaultEmailError = 'Error email',
+        defaultEmailError = 'Unacceptable email',
+        defaultCheckboxMess = 'Checkbox is required',
         defaultEmptyPassword = 'Please insert your password',
-        defaultPasswordLengthError = 'Password must be a longer as '+minLength+' symbols',
+        defaultPasswordLengthError = 'The password must not be shorter than '+minLength+' symbols',
         defaultUnmatchError = 'Passwords do not match',
-        defaultPasswordError = 'Password invalid';
+        defaultPasswordError = 'Unacceptable password',
+        defaultColorErrorMess = "brown";
+    try {
+        password2ErrorOld = check.password2Error.innerText;
+    }
+    catch (err){}
+    try {
+        password2 = check.password2;
+    }
+    catch(err){}
+    try {
+        checkbox = check.checkbox;
+    }
+    catch(err){}
+
     buttonDisabled(true);
     checkEmail();
     checkPassword1();
-    checkPassword2();
+    if (password2 !== null) {
+        checkPassword2();
+    }
     checkSend(check.button);
 
-    function checkSend(input) {
-        document.addEventListener('DOMContentLoaded',function() {
-            input.onclick=clickSend;
+    function checkSend(input)
+    {
+        document.addEventListener('click',function() {
+            if (check.password2) {
+                input.onclick = clickSend;
+            }
+            else {
+                input.onclick = click;
+            }
         },false);
     }
+    function click() {
+        check.callbackSend(check.email.value, check.password.value);
+    }
     function clickSend() {
-        if (check.email.value === ''){
+        check.buttonError.style.color = check.colorErrorMesages || defaultColorErrorMess;
+        if (check.email.value === emailErrorOld){
             check.buttonError.innerText = check.passwordErrorMessageIfEmpty || defaultEmptyEmail;
         }
         else if (!check.email.value.match(pregEmail)){
             check.buttonError.innerText = check.emailError.message || defaultEmailError
         }
-        else if (check.password1.value === ''){
+        else if (check.password.value === passwordErrorOld){
             check.buttonError.innerText = check.passwordErrorMessageIfEmpty || defaultEmptyPassword;
         }
-        else if (!check.password1.value.match(pregPass)){
+        else if (!check.password.value.match(pregPass)){
             check.buttonError.innerText = check.passwordErrorMessage || defaultPasswordError;
         }
-        else if (check.password1.value.length <= check.minPasswordLength ){
+        else if (check.password.value.length < check.minPasswordLength ){
             check.buttonError.innerText = check.passwordLengthMessage || defaultPasswordLengthError;
         }
         else {
-            check.callbackSend(check.email.value, check.password1.value, check.password2.value);
+            if (check.checkbox !== null){
+                if (checkbox.checked === false){
+                    check.buttonError.innerText = check.checkboxMessage || defaultCheckboxMess;
+                }
+                else {
+                    check.buttonError.innerText = buttonErrorOld;
+                    check.callbackSend(check.email.value, check.password.value, check.password2.value);
+                }
+            }
+            else {
+                check.buttonError.innerText = buttonErrorOld;
+                check.callbackSend(check.email.value, check.password.value, check.password2.value);
+            }
         }
     }
     function buttonDisabled(bool) {
@@ -93,28 +145,28 @@ function c(check)
           error = check.emailError,
           message = check.emailError.message  || defaultEmailError,
           messageIfEmpty = check.emailError.messageIfEmpty  || defaultEmptyEmail;
-        listener(input, error, pregEmail, message, messageIfEmpty);
+        listener(input, error, pregEmail, message, messageIfEmpty, emailErrorOld);
         inputer(input);
     }
 
     function checkPassword1() {
-        let input = check.password1,
-            error = check.password1Error,
+        let input = check.password,
+            error = check.passwordError,
             message = check.passwordErrorMessage  || defaultPasswordError,
             messageIfEmpty = check.passwordErrorMessageIfEmpty  || defaultEmptyPassword;
 
-        listener(input, error, pregPass, message, messageIfEmpty, 1);
+        listener(input, error, pregPass, message, messageIfEmpty, passwordErrorOld, 1);
         inputer(input);
     }
 
     function checkPassword2() {
         let input = check.password2,
             error = check.password2Error,
-            message = check.passwordErrorMessage  || 'Password invalid',
+            message = check.passwordErrorMessage  || defaultPasswordError,
             preg = /\w+/,
             messageIfEmpty = check.passwordErrorMessageIfEmpty  || defaultEmptyPassword;
 
-        listener(input, error, preg, message, messageIfEmpty, 2);
+        listener(input, error, preg, message, messageIfEmpty, password2ErrorOld, 2);
         inputer(input);
     }
     function inputer(input)
@@ -129,15 +181,21 @@ function c(check)
         }
         else {
             if (check.email.value.match(pregEmail)) {
-                if (check.password1Error.innerText === '') {
-                    if (check.password1.value === check.password2.value) {
-                        if (check.password1.value !== '' && check.password2.value !== '') {
-                            buttonDisabled(false);
-                            check.buttonError.innerText = '';
+                if (check.passwordError.innerText === passwordErrorOld) {
+                    if (check.password.value.length >= check.minPasswordLength) {
+                        if (check.password2 !== null) {
+                            if (check.password.value === check.password2.value) {
+                                buttonDisabled(false);
+                                check.buttonError.innerText = buttonErrorOld;
+                            }
+                            else {
+                                buttonDisabled(true);
+                            }
                         }
                         else {
-                            buttonDisabled(true);
-                        }
+                                buttonDisabled(false);
+                                check.buttonError.innerText = buttonErrorOld;
+                            }
                     }
                     else {
                         buttonDisabled(true);
@@ -153,7 +211,7 @@ function c(check)
         }
     }
 
-    function listener(input, error, preg, message, messageIfEmpty, index = 0) {
+    function listener(input, error, preg, message, messageIfEmpty, old, index = 0) {
         document.addEventListener('DOMContentLoaded',function() {
             input.onchange=changeEventHandler;
         },false);
@@ -168,9 +226,9 @@ function c(check)
             else {
                 if (index === 1){
                     let length = event.target.value.length;
-                    if (length <= minLength)
+                    if (length < minLength)
                     {
-                        prg = /3443ubercase212/;
+                        prg = /ubercasenomutchchalenge/;
                         msg = check.passwordLengthMessage || defaultPasswordLengthError;
                     }
                     else {
@@ -179,9 +237,9 @@ function c(check)
                     }
                 }
                 if (index === 2){
-                    if (check.password1.value !== check.password2.value)
+                    if (check.password.value !== check.password2.value)
                     {
-                        prg = /3443ubercase212/;
+                        prg = /ubercasenomutchchalenge/;
                         msg = check.passwordsUnmatchMessage || defaultUnmatchError;
                     }
                     else {
@@ -189,18 +247,20 @@ function c(check)
                         msg = message;
                     }
                 }
-                changer(input, error, prg, msg);
+                changer(input, error, prg, msg, old);
             }
         }
     }
 
-    function changer(input, error, preg, message)
+    function changer(input, error, preg, message, old)
     {
         if (input.value.match(preg)) {
-            error.innerText = '';
+            error.innerText = old;
+            error.style.color = passwordColorOld;
         }
         else {
             error.innerText = message;
+            error.style.color = check.colorErrorMesages || defaultColorErrorMess;
         }
     }
 }
